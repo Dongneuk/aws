0ad13a4cb232e6076992b31c696a8ca31eac6ca7
Dongneuk Kim <eastkim9670@gmail.com>
2022-12-07 00:23:45 +0900

    그래이들 변경

--- src/test/java/com/example/awsexercise/HelloControllerTest.java
+++ src/test/java/com/example/awsexercise/HelloControllerTest.java
@@ -1,12 +1,12 @@
 package com.example.awsexercise;
 
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,3 +27,4 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 }
+
